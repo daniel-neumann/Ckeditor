154c5fd430b5507d68bd4443dbcdfb37db08c766
Rachman Chavik <rchavik@xintesa.com>
2013-11-18 09:03:15 +0700

    Removing other plugin's config

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -7,4 +7,3 @@ foreach ((array)Configure::read('Wysiwyg.actions') as $action => $settings) {
 	$actionE = explode('/', $action);
 	Croogo::hookHelper($actionE['0'], 'Ckeditor.Ckeditor');
 }
-Croogo::hookHelper('Attachments', 'Ckeditor.Ckeditor');
